4bbab365fade99ad965c02e44ca5336fe5fb639a
davidvera13 <david.vera@9online.fr>
2023-01-17 21:49:53 +0100

    feat(java reactive): Mono block

---
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec01/Lec11Block.java
@@ -0,0 +1,38 @@
+package com.reactive.sec01;
+
+import com.reactive.utils.Utils;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec11Block {
+    public static void main(String[] args) {
+
+        // behavior is different because of Schedulers
+        getName();
+        String name = getName()
+                .subscribeOn(Schedulers.boundedElastic())
+                .block();
+
+        System.out.println(name);
+        getName();
+
+//        getName()
+//                .subscribeOn(Schedulers.boundedElastic())
+//                .subscribe(Utils.onNext());
+//        getName();
+//
+//        // we do not see any result, we can use sleep
+//        Utils.sleepSeconds(4);
+
+    }
+
+
+    private static Mono<String> getName() {
+        System.out.println("getName() was called");
+        return Mono.fromSupplier(() -> {
+            System.out.println("Generating name ... ");
+            Utils.sleepSeconds(3);
+            return Utils.faker().name().fullName();
+        }).map(String::toUpperCase);
+    }
+}
