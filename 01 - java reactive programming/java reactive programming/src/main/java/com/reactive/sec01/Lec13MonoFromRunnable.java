758dea08046b1ff77d8bfe731a211a1a73346bcd
davidvera13 <david.vera@9online.fr>
2023-01-17 22:15:49 +0100

    feat(java reactive): Mono from runnable

---
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec01/Lec13MonoFromRunnable.java
@@ -0,0 +1,34 @@
+package com.reactive.sec01;
+
+import com.reactive.utils.Utils;
+import reactor.core.publisher.Mono;
+
+public class Lec13MonoFromRunnable {
+    public static void main(String[] args) {
+        // it takes no parameter and return nothing.
+        // When to use runnable ? We may have operation that take some time and we expect a notification.
+        Runnable runnable = () -> System.out.println("Something happens...");
+        Mono.fromRunnable(runnable)
+                .subscribe(
+                        Utils.onNext(),
+                        Utils.onError(),
+                        Utils.onComplete());
+
+        // if calling time consumming method
+        Mono.fromRunnable(timeConsumingProcess())
+                .subscribe(
+                        Utils.onNext(),
+                        Utils.onError(),
+                        () -> System.out.println("Process completed... Sending mails to whole team"));
+    }
+
+
+    private static Runnable timeConsumingProcess() {
+        return () -> {
+            Utils.sleepSeconds(3);
+            System.out.println("We took time to do process");
+        };
+    }
+
+
+}
