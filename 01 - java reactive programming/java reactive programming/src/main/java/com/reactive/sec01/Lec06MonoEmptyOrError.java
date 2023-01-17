40cfefa48d1eaf4a5be37a6df2ce16aef4beb474
davidvera13 <david.vera@9online.fr>
2023-01-17 18:54:11 +0100

    feat(java reactive); Mono empty or error

---
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec01/Lec06MonoEmptyOrError.java
@@ -0,0 +1,45 @@
+package com.reactive.sec01;
+
+import com.reactive.utils.Utils;
+import reactor.core.publisher.Mono;
+
+public class Lec06MonoEmptyOrError {
+    public static void main(String[] args) {
+        userRepository(1)
+                .subscribe(
+                        Utils.onNext(),
+                        Utils.onError(),
+                        Utils.onComplete());
+
+        // directly reach completed, userRepository return empty
+        userRepository(2)
+                .subscribe(
+                        Utils.onNext(),
+                        Utils.onError(),
+                        Utils.onComplete());
+
+
+        // directly throws exception
+        userRepository(3)
+                .subscribe(
+                        Utils.onNext(),
+                        Utils.onError(),
+                        Utils.onComplete());
+
+
+    }
+
+
+    // faking repository
+    private static Mono<String> userRepository(int userId) {
+        if (userId == 1)
+            return Mono.just(Utils.faker().name().fullName());
+        else if(userId == 2)
+            return Mono.empty();
+            // return empty, not null
+            // return null;
+        else
+            return Mono.error(new RuntimeException("Out of allowed user ranges..."));
+
+    }
+}
