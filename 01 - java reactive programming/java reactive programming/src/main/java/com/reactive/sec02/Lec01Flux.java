e9c50983ac7510cb7e3ab6d102bf50b914bb5a36
davidvera13 <david.vera@9online.fr>
2023-01-18 21:45:31 +0100

    feat(java reactive): Flux introduction

--- 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec02/Lec01Flux.java
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec02/Lec01Flux.java
@@ -1,2 +1,24 @@
-package com.reactive.sec02;public class Lec01Flux {
+package com.reactive.sec02;
+
+import com.reactive.utils.Utils;
+import reactor.core.publisher.Flux;
+
+public class Lec01Flux {
+    public static void main(String[] args) {
+        // 0..1..n
+        Flux<Integer> integerFlux = Flux.just(1);
+        integerFlux.subscribe(Utils.onNext());
+
+        System.out.println("*************");
+        Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5);
+        flux.subscribe(Utils.onNext(), Utils.onError(), Utils.onComplete());
+
+        System.out.println("*************");
+        Flux<Integer> emptyFlux = Flux.empty();
+        emptyFlux.subscribe(Utils.onNext(), Utils.onError(), Utils.onComplete());
+
+        System.out.println("*************");
+        Flux<Object> objectFlux = Flux.just(1, 2, 3, "Hello World", Utils.faker().aquaTeenHungerForce().character());
+        objectFlux.subscribe(Utils.onNext(), Utils.onError(), Utils.onComplete());
+    }
 }
