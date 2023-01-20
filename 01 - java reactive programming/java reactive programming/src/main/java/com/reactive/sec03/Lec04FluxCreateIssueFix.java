366b125e85afda34e441360b489504c395f7e683
davidvera13 <david.vera@9online.fr>
2023-01-20 20:18:08 +0100

    feat(java reactive): Flux generate

--- 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec03/Lec04FluxCreateIssueFix.java
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec03/Lec04FluxCreateIssueFix.java
@@ -5,6 +5,7 @@ import reactor.core.publisher.Flux;
 
 public class Lec04FluxCreateIssueFix {
     public static void main(String[] args) {
+        // only one instance of flux sink will be generated
         Flux
                 .create(fluxSink -> {
                     String country;
@@ -19,5 +20,34 @@ public class Lec04FluxCreateIssueFix {
                 })
                 .take(3) // subscriber stops receiving, but we're still emitting ...
                 .subscribe(Utils.subscriber());
+
+        System.out.println("********************************************************");
+        // will emit until
+        Flux
+                .create(fluxSink -> {
+                    String country;
+                    do {
+                        country = Utils.faker().country().name();
+                        System.out.println("emitting -> " + country);
+                        fluxSink.next(country);
+                    } while (!country.equalsIgnoreCase("canada"));
+                    fluxSink.complete();
+                })
+                .subscribe(Utils.subscriber());
+
+        System.out.println("********************************************************");
+
+        // with flux generate
+        Flux
+                .generate(synchronousSink -> {
+                    String country = Utils.faker().country().name();
+                    System.out.println("emitting -> " + country);
+                    synchronousSink.next(country);
+                    if(country.equalsIgnoreCase("canada"))
+                        synchronousSink.complete();
+                })
+                // .take(3)
+                .subscribe(Utils.subscriber());
+
     }
 }
