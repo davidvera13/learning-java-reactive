dfc5dd47edb07e5c3f5a9e7b8c02162c2fc6d93d
davidvera13 <david.vera@9online.fr>
2023-01-22 17:08:56 +0100

    feat(java reactive): publish on

---
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec06ThreadingScheduler/Lec04PublishOn.java
@@ -0,0 +1,32 @@
+package com.reactive.sec06ThreadingScheduler;
+
+import com.reactive.utils.Utils;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec04PublishOn {
+    public static void main(String[] args) {
+        Flux<Object> flux = Flux
+                .create(fluxSink -> {
+                    printThreadName("Flux.create() called");
+                    for (int i = 0; i < 5; i++) {
+                        fluxSink.next(i);
+                    }
+                    fluxSink.complete();
+                })
+                .doOnNext(i -> printThreadName("next " + i));
+
+
+        flux
+                .publishOn(Schedulers.boundedElastic())
+                .doOnNext(i -> printThreadName("next " + i))
+                .publishOn(Schedulers.parallel())
+                .subscribe(v -> printThreadName("sub " + v));
+
+        Utils.sleepSeconds(5);
+    }
+
+    private static void printThreadName(String message) {
+        System.out.println("> Thread: " + Thread.currentThread().getName() + " > \t " + message);
+    }
+}
