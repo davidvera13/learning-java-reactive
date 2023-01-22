3bbd4e46760187404c2478a79fa889dbc67ca806
davidvera13 <david.vera@9online.fr>
2023-01-22 20:37:34 +0100

    feat(java reactive): start with

--- 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec08PublishersCombinations/Lec01StartWith.java
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec08PublishersCombinations/Lec01StartWith.java
@@ -1,2 +1,39 @@
-package com.reactive.sec08PublishersCombinations;public class Lec01StartWith {
+package com.reactive.sec08PublishersCombinations;
+
+import com.reactive.sec08PublishersCombinations.helper.NameGenerator;
+import com.reactive.utils.Utils;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lec01StartWith {
+    public static void main(String[] args) {
+
+
+        NameGenerator nameGenerator = new NameGenerator();
+        nameGenerator
+                .generateNames()
+                .take(5)
+                .subscribe(Utils.subscriber("First Subscriber"));
+
+        // for 2nd subscription, we use cached values, except if new items are required (between 6 & 8)
+        nameGenerator
+                .generateNames()
+                .take(8)
+                .subscribe(Utils.subscriber("Second Subscriber"));
+
+        // will generate one more
+        nameGenerator
+                .generateNames()
+                .take(9)
+                .subscribe(Utils.subscriber("Third Subscriber"));
+
+        // will generate one more
+        nameGenerator
+                .generateNames()
+                .filter(name -> name.startsWith("A"))
+                .take(1)
+                .subscribe(Utils.subscriber("Fourth Subscriber"));
+    }
 }
--- 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec08PublishersCombinations/helper/NameGenerator.java
+++ 01 - java reactive programming/java reactive programming/src/main/java/com/reactive/sec08PublishersCombinations/helper/NameGenerator.java
@@ -1,28 +1,31 @@
-package com.reactive.sec02Flux.helper;
+package com.reactive.sec08PublishersCombinations.helper;
 
 import com.reactive.utils.Utils;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NameGenerator {
-    public static String getName() {
-        Utils.sleepSeconds(1);
-        return Utils.faker().name().fullName();
+
+    List<String> names = new ArrayList<>();
+
+    public Flux<String> generateNames() {
+        return Flux
+                .generate((SynchronousSink<String> synchronousSink) -> {
+                    System.out.println("Generate name");
+                    Utils.sleepSeconds(1);
+                    String name = Utils.faker().name().fullName();
+                    // caching the names in a list to reuse the values
+                    names.add(name);
+                    synchronousSink.next(name);
+                })
+                .cast(String.class)
+                .startWith(getNamesFromCache());
     }
 
-    public static List<String> getNames(int count) {
-        List<String> list = new ArrayList<>(count);
-        for (int i = 0; i < count ; i++) {
-            list.add(getName());
-        }
-        return list;
-    }
-
-    public static Flux<String> getFluxNames(int count) {
-        // for int i = 0 to count, call getName() into a Flux...
-        return Flux.range(0, count)
-                .map(i -> getName());
+    private Flux<String> getNamesFromCache() {
+        return Flux.fromIterable(names);
     }
 }
