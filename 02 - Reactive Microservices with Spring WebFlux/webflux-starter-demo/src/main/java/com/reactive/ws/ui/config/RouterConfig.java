daf185b85b443129c3e3fe3c49c2cc075f645523
davidvera13 <david.vera@9online.fr>
2023-01-26 00:14:47 +0100

    feat(reactive-ws): splitting routerFunction

--- 02 - Reactive Microservices with Spring WebFlux/webflux-starter-demo/src/main/java/com/reactive/ws/ui/config/RouterConfig.java
+++ 02 - Reactive Microservices with Spring WebFlux/webflux-starter-demo/src/main/java/com/reactive/ws/ui/config/RouterConfig.java
@@ -25,6 +25,7 @@ public class RouterConfig {
     @Bean
     public RouterFunction<ServerResponse> serverResponseRouterFunction() {
         return RouterFunctions.route()
+                .path("customUrl", this::serverResponseCustomUrlRouterFunction)
                 .GET("router/square/{input}", requestHandler::squareHandler)
                 .GET("router/multiplication/{input}", requestHandler::tableHandler)
                 .GET("router/multiplication/{input}/stream", requestHandler::tableStreamingHandler)
@@ -33,6 +34,36 @@ public class RouterConfig {
                 .onError(InputValidationException.class, exceptionHandler())
                 .build();
     }
+
+    // create sub "routes
+    private RouterFunction<ServerResponse> serverResponseCustomUrlRouterFunction() {
+        /// the routes will start with /customUrl
+        return RouterFunctions.route()
+                .GET("square/{input}", requestHandler::squareHandler)
+                .GET("multiplication/{input}", requestHandler::tableHandler)
+                .GET("multiplication/{input}/stream", requestHandler::tableStreamingHandler)
+                .POST("operation", requestHandler::doOperationHandler)
+                .GET("squareWithValidation/{input}", requestHandler::squareWithValidationHandler)
+                .onError(InputValidationException.class, exceptionHandler())
+                .build();
+    }
+
+
+
+
+    // we can have as many handler ... we must declare bean every time
+    @Bean
+    public RouterFunction<ServerResponse> serverResponseOtherRouterFunction() {
+        return RouterFunctions.route()
+                .GET("other/square/{input}", requestHandler::squareHandler)
+                .GET("other/multiplication/{input}", requestHandler::tableHandler)
+                .GET("other/multiplication/{input}/stream", requestHandler::tableStreamingHandler)
+                .POST("other/operation", requestHandler::doOperationHandler)
+                .GET("other/squareWithValidation/{input}", requestHandler::squareWithValidationHandler)
+                .onError(InputValidationException.class, exceptionHandler())
+                .build();
+    }
+
 
     private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler()  {
         // we use error and request
