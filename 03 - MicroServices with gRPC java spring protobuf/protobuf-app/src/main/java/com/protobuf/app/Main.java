cf1484e707e07fb33d841784136041fb938154c2
davidvera13 <david.vera@9online.fr>
2023-02-04 15:00:29 +0100

    feat(grpc): serialization / deserialization

--- 03 - MicroServices with gRPC java spring protobuf/protobuf-app/src/main/java/com/protobuf/app/Main.java
+++ 03 - MicroServices with gRPC java spring protobuf/protobuf-app/src/main/java/com/protobuf/app/Main.java
@@ -2,8 +2,13 @@ package com.protobuf.app;
 
 import com.protobuf.app.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // using generated claas
         Person person = Person.newBuilder()
                 .setAge(22)
@@ -19,5 +24,15 @@ public class Main {
 
         // equality between objects
         System.out.println(person.equals(otherperson));
+
+        // serialization / deserialisation
+        Path path = Paths.get("user.ser");
+        Files.write(path, person.toByteArray());
+        System.out.println("********");
+
+        byte[] bytes = Files.readAllBytes(path);
+        Person p = Person.parseFrom(bytes);
+        System.out.println(p);
+
     }
 }
