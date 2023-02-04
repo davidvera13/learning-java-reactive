93731499f9b05d60d127b9f449b082efec612a3e
davidvera13 <david.vera@9online.fr>
2023-02-04 14:51:39 +0100

    feat(grpc): protobuf class implementation in main class

--- 03 - MicroServices with gRPC java spring protobuf/protobuf-app/src/main/java/com/protobuf/app/Main.java
+++ 03 - MicroServices with gRPC java spring protobuf/protobuf-app/src/main/java/com/protobuf/app/Main.java
@@ -1,7 +1,23 @@
 package com.protobuf.app;
 
+import com.protobuf.app.models.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // using generated claas
+        Person person = Person.newBuilder()
+                .setAge(22)
+                .setName("Walter")
+                .build();
+
+        Person otherperson = Person.newBuilder()
+                .setAge(22)
+                .setName("Walter")
+                .build();
+
+        System.out.println(person);
+
+        // equality between objects
+        System.out.println(person.equals(otherperson));
     }
 }
