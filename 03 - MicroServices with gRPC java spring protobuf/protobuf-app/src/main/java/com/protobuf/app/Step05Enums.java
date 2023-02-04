d39a60fa34c1c848effb2827534cc9e7bb93d20b
davidvera13 <david.vera@9online.fr>
2023-02-04 17:31:41 +0100

    feat(grpc): enums

---
+++ 03 - MicroServices with gRPC java spring protobuf/protobuf-app/src/main/java/com/protobuf/app/Step05Enums.java
@@ -0,0 +1,29 @@
+package com.protobuf.app;
+
+import com.protobuf.app.models.BodyStyle;
+import com.protobuf.app.models.Car;
+import com.protobuf.app.models.CarDealer;
+
+public class Step05Enums {
+    public static void main(String[] args) {
+
+        CarDealer carDealer = CarDealer.newBuilder()
+                .putModels(2020, Car.newBuilder()
+                        .setMake("MG")
+                        .setModel("TF 1250")
+                        .setYear(1954)
+                        .build())
+                .putModels(2021, Car.newBuilder()
+                        .setMake("Lotus")
+                        .setModel("Seven")
+                        .setYear(1958)
+                        .setBodyStyle(BodyStyle.SUV)
+                        .build())
+                .build();
+
+        // default value is set to 0 => here UNDEFINED is default
+        System.out.println(carDealer.getModelsOrThrow(2020).getBodyStyle());
+        System.out.println(carDealer.getModelsOrThrow(2021).getBodyStyle());
+
+    }
+}
