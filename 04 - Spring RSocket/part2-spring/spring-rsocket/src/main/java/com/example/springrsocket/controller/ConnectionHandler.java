5942ed287f7d624ccf72591716a991688784c821
davidvera13 <david.vera@9online.fr>
2023-03-27 22:04:42 +0200

    feat(rsocket spring): disposing connections

--- 04 - Spring RSocket/part2-spring/spring-rsocket/src/main/java/com/example/springrsocket/controller/ConnectionHandler.java
+++ 04 - Spring RSocket/part2-spring/spring-rsocket/src/main/java/com/example/springrsocket/controller/ConnectionHandler.java
@@ -3,6 +3,7 @@ package com.example.springrsocket.controller;
 import com.example.springrsocket.shared.dto.ClientConnectionRequestDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
@@ -15,16 +16,31 @@ public class ConnectionHandler {
 //        return Mono.empty();
 //    }
 
-    // we can also pass an object
+//    // we can also pass an object
+//    @ConnectMapping
+//    public Mono<Void> handleConnection(ClientConnectionRequestDto clientConnectionRequestDto) {
+//        System.out.println("Connection setup");
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(clientConnectionRequestDto));
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return clientConnectionRequestDto.getClientSecret().equals("password") ? Mono.empty() : Mono.error(new RuntimeException("Invalid credentials"));
+//    }
+
+    // we can also pass an object & rSocketRequest from client and dispose requester
     @ConnectMapping
-    public Mono<Void> handleConnection(ClientConnectionRequestDto clientConnectionRequestDto) {
+    public Mono<Void> handleConnection(ClientConnectionRequestDto clientConnectionRequestDto, RSocketRequester requester) throws JsonProcessingException {
         System.out.println("Connection setup");
         ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(clientConnectionRequestDto));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        return clientConnectionRequestDto.getClientSecret().equals("password") ? Mono.empty() : Mono.error(new RuntimeException("Invalid credentials"));
+
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(clientConnectionRequestDto));
+
+        System.out.println("*************");
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(requester));
+        return clientConnectionRequestDto.getClientSecret().equals("password") ?
+                Mono.empty() :
+                Mono.fromRunnable(() -> requester.rsocketClient().dispose()); // ClosedChannelException
     }
 }
